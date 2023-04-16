23be460431b7da9a435c4fea434851a1f3edf9c2
Ravi <rp97.reddy@gmail.com>
2023-04-17 00:40:10 +0530

    mongodb installation

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,4 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-** edit the file and replace 127.0.0.0 with 0.0.0.0
+** edit the file and replace 127.0.0.1 with 0.0.0.0
