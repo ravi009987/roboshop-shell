ef70fe4e71b4bb28d0da1f0b7688efd699fbb0fb
Ravi <rp97.reddy@gmail.com>
2023-04-19 23:17:46 +0530

    Adding paths

--- catalogue.sh
+++ catalogue.sh
@@ -33,7 +33,7 @@ systemctl restart catalogue
 
 
 echo -e "\e[36m>>>>> Copy mongo repo <<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>> Install mongodb client <<<<\e[0m"
 yum install mongodb-org-shell -y
